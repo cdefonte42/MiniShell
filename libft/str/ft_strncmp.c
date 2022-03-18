76ca0ef896817bdd7686bc7e2967b57d53009277
Marius BRAETS <mbraets@student.42.fr>
2022-03-18 11:39:12 +0100

    Segfault fixed

--- libft/str/ft_strncmp.c
+++ libft/str/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbraets <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: mbraets <mbraets@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 16:10:45 by mbraets           #+#    #+#             */
-/*   Updated: 2021/11/23 17:42:24 by mbraets          ###   ########.fr       */
+/*   Updated: 2022/03/18 11:38:43 by mbraets          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	if (!s1 || !s2)
+		return (0);
 	i = 0;
 	while (i < n && s1[i] == s2[i] && s1[i] && s2[i])
 		i++;
