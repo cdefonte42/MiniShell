1f45df3e92ffc1ec56fd4724309f97d43466c7ca
cdefonte <cdefonte@student.42.fr>
2022-04-05 13:01:49 +0200

    nieh

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: cdefonte <cdefonte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 11:51:24 by cdefonte          #+#    #+#             */
-/*   Updated: 2022/04/05 12:42:43 by cdefonte         ###   ########.fr       */
+/*   Updated: 2022/04/05 12:58:50 by cdefonte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,19 @@ int	ft_get_tokens(t_list **token_lst, char *s)
 	while (s && s[i])
 	{
 		while (s && s[i] != '\'' && s[i] != '"' && !ft_ismetachar(s[i]))
-		{
-			token_start++;
 			i++;
-		}
 		if (token_start - i != 0)
 		{
 			tmp = ft_substr(s, token_start, i - token_start);
 			if (!tmp)
 				return (printf("substr failed\n"), -1); //et free stuff
 		}
+		newtoken = ft_lstnew(&tmp);
+		free(tmp);
+		if (!newtoken)
+			return (printf("lstnew failed\n", -1);
+		ft_lstadd_back(token_lst, newtoken);
+		token_start = i;
 		
 
 		i++;
