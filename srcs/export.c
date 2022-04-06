a829459d86b4ac1c5b334882a7b8f5f075c0770f
Marius BRAETS <mbraets@student.42.fr>
2022-04-06 14:35:00 +0200

    starting add value

--- srcs/export.c
+++ srcs/export.c
@@ -6,7 +6,7 @@
 /*   By: mbraets <mbraets@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 10:04:56 by cdefonte          #+#    #+#             */
-/*   Updated: 2022/04/06 13:06:20 by mbraets          ###   ########.fr       */
+/*   Updated: 2022/04/06 13:21:54 by mbraets          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	ft_add_back(t_var **alst, t_var *new)
 		*alst = new;
 		return ;
 	}
+	last = *alst;
 	while (last->next)
 		last = last->next;
 	last->next = new;
@@ -40,21 +41,36 @@ t_var	*var_getfromkey(t_var *var_list, char *key)
 	return (head);
 }
 
+int	ft_set_var(t_var **var_lst, char *str)
+{
+
+}
+
 /* Export la variable declaree dans str avec sa value si str contient '=' */
 int	ft_add_var(t_var **var_lst, char *str)
 {
 	t_var	*new_var;
+	t_var	*old_var;
 	char	**value;
+	char	*new;
 	
 	value = ft_split(str, '=');
 	if (!value)
 		return (-1);
-	new_var = malloc(sizeof(t_var));
-	if (!new_var)
-		return (free(value), free(value[0]), free(value[1]), -1);
-	*new_var = (t_var) {.key = value[0], .value = value[1]};
+	old_var = var_getfromkey(*var_lst, value[0]);
+	if (old_var == NULL)
+	{
+		new_var = malloc(sizeof(t_var));
+		if (!new_var)
+			return (free(value), free(value[0]), free(value[1]), -1);
+		*new_var = (t_var) {.key = value[0], .value = value[1]};
+		ft_add_back(var_lst, new_var);
+	}
+	else
+	{
+		new = ft_strdup(old_var->value);
+	}
 	free(value);
-	ft_add_back(var_lst, new_var);
 	return (0);
 }
 
