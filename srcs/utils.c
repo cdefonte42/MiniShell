07c6a4dd93361534e0a73a597b728cb2e266af1c
cdefonte <cdefonte@student.42.fr>
2022-04-26 18:37:29 +0200

    ajout retour a la ligne print pour ft_error

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: mbraets <mbraets@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 10:03:20 by cdefonte          #+#    #+#             */
-/*   Updated: 2022/04/26 12:58:37 by cdefonte         ###   ########.fr       */
+/*   Updated: 2022/04/26 18:19:23 by cdefonte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -144,5 +144,6 @@ void	ft_error(char *prefix, char *suffix)
 		ft_putstr_fd(": ", 2);
 		ft_putstr_fd(suffix, 2);
 	}
+	ft_putstr_fd("\n", 2);
 }
 
