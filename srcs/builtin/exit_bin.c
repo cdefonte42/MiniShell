b48e86e943006f3301c1bea3a57abb3b23f00dc0
Marius BRAETS <mbraets@student.42.fr>
2022-05-12 17:14:04 +0200

    fix exit double free segfault on too many arguments

--- srcs/builtin/exit_bin.c
+++ srcs/builtin/exit_bin.c
@@ -22,7 +22,7 @@ int	ft_exitbin(t_minishell *msh, char **cmd)
 	if (cmd[1] != NULL)
 	{
 		if (cmd[2])
-			return (free(cmd), ft_error("too many arguments", NULL), 2);
+			return (ft_error("too many arguments", NULL), 1);
 		if (ft_stris(cmd[1], ft_isdigit) && ft_sli(cmd[1]) < 19)
 			ret_stat = ft_atoi(cmd[1]);
 		else
