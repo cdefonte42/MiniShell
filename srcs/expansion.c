6b0f2d244e71e54d2e54eecf6ba8e4c3bd04917c
cdefonte <cdefonte@student.42.fr>
2022-05-16 12:55:29 +0200

    clearing trucs commentes inutiles

--- srcs/expansion.c
+++ srcs/expansion.c
@@ -6,7 +6,7 @@
 /*   By: mbraets <mbraets@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 14:10:15 by mbraets           #+#    #+#             */
-/*   Updated: 2022/05/11 14:50:06 by cdefonte         ###   ########.fr       */
+/*   Updated: 2022/05/16 12:53:30 by cdefonte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,40 +67,6 @@ int	expand_str(char **str, t_var *var_lst)
 	}
 	return (SUCCESS);
 }
-
-// int	expand_str(char **str, t_var *var_lst)
-// {
-// 	int		i;
-// 	int		inquote;
-// 	char	*kval[2];
-
-// 	i = 0;
-// 	inquote = nil;
-// 	while (str && *str && (*str)[i])
-// 	{
-// 		kval[0] = NULL;
-// 		kval[1] = NULL;
-// 		set_curr_quote((*str)[i], &inquote);
-// 		if (inquote != singleq && (*str)[i] == '$' && (*str)[i + 1])
-// 		{
-// 			if ((*str)[i + 1] == '\'' || (*str)[i + 1] == '"')
-// 			{
-// 				if (exp_replacewith(str, i, "$", NULL) == FAILURE)
-// 					return (FAILURE);
-// 			}
-// 			else if (get_dolls(*str + i, &kval[0]) == FAILURE
-// 				|| get_value(kval[0], &kval[1], var_lst, inquote) == FAILURE
-// 				|| exp_replacewith(str, i, kval[0], kval[1]) == FAILURE)
-// 				return (free(kval[0]), free(kval[1]), FAILURE);
-// 			i += expand_add(kval[0], kval[1]);
-// 			free(kval[0]);
-// 			free(kval[1]);
-// 		}
-// 		else
-// 			i++;
-// 	}
-// 	return (SUCCESS);
-// }
 
 int	msh_split_token(t_token *token)
 {
